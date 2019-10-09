fccfeb171c2efc98f9bf71a0be53eb41ffa810d2
pfdzm <pfdzm@users.noreply.github.com>
2019-10-09 15:53:39 +0200

    add buttons to pick question pool, prevent default event

---
+++ javascript/questions.js
@@ -0,0 +1,19 @@
+var questionsEasy = [
+  {
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
+  }
+  ///etc.
+];
+
+const questionsHard = [
+  { title: "A", choices: ["a", "b", "c"], answer: "c" },
+  { title: "B", choices: ["a", "b", "c"], answer: "a" },
+  { title: "C", choices: ["a", "b", "c"], answer: "b" }
+];
--- javascript/script.js
+++ javascript/script.js
@@ -1,13 +1,14 @@
 class CodeQuiz {
-  constructor(level) {
-    this.level = level;
+  constructor() {
     this.timerId = document.querySelector("#timer");
     this.timer = 0;
     this.nIntervId;
   }
-  start() {
+  start(questions) {
+    this.questions = questions;
     document.querySelector("#app").innerHTML = `
       <h1 class="text-center">Welcome to Code Quiz</h1>`;
+    this.startTimer();
   }
 
   startTimer() {
@@ -15,7 +16,7 @@ class CodeQuiz {
       this.timerId.textContent = ++this.timer;
       if (this.timer == 10) {
         document.querySelector("#app").innerHTML += `
-        <h1>Time's up!</h1>`;
+        <h1 class="text-center">Time's up!</h1>`;
         this.stopTimer();
       }
     }, 1000);
@@ -24,9 +25,19 @@ class CodeQuiz {
     clearInterval(this.nIntervId);
   }
 }
-const quiz = new CodeQuiz("easy");
 
-document.querySelector("#start").addEventListener("click", () => {
-  quiz.start();
-  quiz.startTimer();
+const quiz = new CodeQuiz();
+
+document.querySelector("form").addEventListener("submit", function(e) {
+  e.preventDefault();
+});
+
+document.querySelector("#startEasy").addEventListener("click", e => {
+  e.preventDefault();
+  quiz.start(questionsEasy);
+});
+
+document.querySelector("#startHard").addEventListener("click", e => {
+  e.preventDefault();
+  quiz.start(questionsHard);
 });
